8ddfe58f46a82a019ec8fb5c828c50c65d93aa0b
Samandeep Singh <samsandhu2213@gmail.com>
2024-12-31 21:29:55 +0530

    Initial commit

---
+++ src/app/MyComponents/com1/com1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-com1',
+  imports: [],
+  templateUrl: './com1.component.html',
+  styleUrl: './com1.component.css'
+})
+export class Com1Component {
+
+}
